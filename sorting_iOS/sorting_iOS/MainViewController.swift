8d29aa02a9dae8d237e4353c21589378ce628a7d
Seoyeong <gjtjdud93@gmail.com>
2018-02-02 16:02:13 +0900

    StoryBoard 제작

---
+++ sorting_iOS/sorting_iOS/MainViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  sorting_iOS
+//
+//  Created by Seoyeong Heo on 2018. 2. 2..
+//  Copyright © 2018년 Seoyeong Heo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var number = [5, 9, 3, 6, 8, 4, 1, 2, 7]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func handlerBubble(_ sender: Any) {
+    }
+    
+    @IBAction func handlerSelection(_ sender: Any) {
+    }
+    
+    @IBAction func handlerInsertion(_ sender: Any) {
+    }
+    
+    @IBAction func handlerMerge(_ sender: Any) {
+    }
+    
+    @IBAction func handlerHeap(_ sender: Any) {
+    }
+    
+    @IBAction func handlerQuick(_ sender: Any) {
+    }
+}
+
